f983022f6e7479e73932cd683c05be726bb0c011
Darkly <146270004+Drakly@users.noreply.github.com>
2025-01-15 18:28:39 +0200

    Initial commit

---
+++ src/main/java/org/darexwallet/DareXWalletApplication.java
@@ -0,0 +1,13 @@
+package org.darexwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DareXWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DareXWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/darexwallet/DareXWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.darexwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DareXWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
